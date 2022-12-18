b41f9697b02db375caff9ade4e1f1171638d289b
VelupriyaMohan <velupriyacse@gmail.com>
2022-12-18 13:09:20 -0800

    appointmentJson class file

---
+++ src/Libraries/Nop.Services/Catalog/appointmentJson.cs
@@ -0,0 +1,11 @@
+﻿namespace Nop.Services.Catalog
+{
+    internal class appointmentJson
+    {
+        public object start { get; set; }
+        public object end { get; set; }
+
+        public object duration { get; set; }
+        public object date { get; set; }
+    }
+}
